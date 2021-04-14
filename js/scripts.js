818a99092387d86ffaf4792f3e399203ae5d9247
andrasbanki <abankiandras@gmail.com>
2021-04-14 17:57:28 +0200

    Created a new variable, added objects and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world')
-
-let favoriteFood = 'Burrito';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']  
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water']
+  }
+];
