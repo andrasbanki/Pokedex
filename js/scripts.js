4ae519ef5b50b1960291810fc7b636378fd1d0af
andrasbanki <abankiandras@gmail.com>
2021-04-16 21:40:30 +0200

    Added a 'for' loop, some comments and conditional statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Array includes a list of Pokémon with some details.
+
 let pokemonList = [
   {
     name: 'Bulbasaur',
@@ -15,3 +17,14 @@ let pokemonList = [
     types: ['water']
   }
 ];
+
+/*This loop is to print a list of Pokémon with name, and height.
+  Print the largest one with a message: "Wow, this is the biggest one!"*/
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 0.7) {
+    document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, this is the biggest one!' + '<br>');
+  } else {
+    document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
+  }
+}
