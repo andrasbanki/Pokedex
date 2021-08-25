5dae28a923901fc0b658ab273bb8a7cd5966743c
andrasbanki <abankiandras@gmail.com>
2021-08-25 14:17:51 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Simple Pokédex JavaScript App
 
-Latest update: 07.05.2021
+![Screenshot](https://github.com/andrasbanki/meet/raw/main/img/Pokedex.png)
+
+Latest update: 25.08.2021
 
 ## Project Description
 
@@ -10,6 +12,12 @@ data from an external API and enables the viewing of data points in detail.
 API link: https://pokeapi.co/api/v2/pokemon/?limit=150
 
 Project link: https://andrasbanki.github.io/Pokedex/
+
+## Key Features
+
+-  Load data from an external source (API)
+-  View a list of items
+-  On user action (e.g., by clicking on a list item), view details for that item
 
 ## Project Dependencies
 
--- src/scripts.js
+++ src/scripts.js
@@ -151,20 +151,20 @@ pokemonRepository.loadList().then(function () {
 
 // Search for pokemons
 
-    // eslint-disable-next-line no-unused-vars
-    function myFunction() {
-      var input, filter, ul, li, a, i, txtValue;
-      input = document.getElementById('searchBar');
-      filter = input.value.toUpperCase();
-      ul = document.getElementById('list-group');
-      li = ul.getElementsByTagName('li');
-      for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName('button')[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = '';
-        } else {
-          li[i].style.display = 'none';
-        }
-      }
+// eslint-disable-next-line no-unused-vars
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchBar');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('list-group');
+  li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName('button')[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
     }
+  }
+}
